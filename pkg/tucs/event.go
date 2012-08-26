f59bc0eefe7e175d99ec71539d6b80c849b6ab91
Sebastien Binet <binet@cern.ch>
2012-08-26 12:56:57 +0200

    make RunList implement sort.Interface

--- pkg/tucs/event.go
+++ pkg/tucs/event.go
@@ -1,5 +1,9 @@
 package tucs
 
+import (
+	"sort"
+)
+
 type Event struct {
 	Run  Run
 	Data map[string]interface{}
@@ -10,5 +14,16 @@ type Run struct {
 	Number int64
 	Data   interface{}
 }
+
+// RunList is a slice of Runs which implements sort.Interface
+type RunList []Run
+
+func (r RunList) Len() int           { return len(r) }
+func (r RunList) Less(i, j int) bool { return r[i].Number < r[j].Number }
+func (r RunList) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+
+func (r RunList) Sort() { sort.Sort(r) }
+
+func SortRunList(runs []Run) { sort.Sort(RunList(runs)) }
 
 // EOF
